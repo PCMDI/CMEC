b4a14799548018c90c05c6bd74c010cff3d4ce8b
Matthew Harris <mattbenharris@gmail.com>
2017-09-07 10:41:14 -0700

    MBH: need to finish sign in, and add content

--- apps/cmec/urls.py
+++ apps/cmec/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^about', views.about, name='about'),
+    url(r'^pmp', views.pmp, name='pmp'),
+    url(r'^ilamb', views.ilamb, name='ilamb'),
+    url(r'^results', views.results, name='results'),
+    url(r'^mean', views.mean, name='mean'),
+    url(r'^variability', views.variability, name='variability'),
     ]
